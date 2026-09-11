66e523270d62b4bae6692a00d30438de73d1a4f4
cdxniel <cbarrionuevof@uteq.edu.ec>
2026-09-10 23:15:51 -0500

    Update data path for processed data

--- 06_Experimento/scripts_analisis/07_tamano_efecto.R
+++ 06_Experimento/scripts_analisis/07_tamano_efecto.R
@@ -27,7 +27,7 @@
 
 set.seed(2026)  # reproducibilidad del bootstrap
 
-ruta_datos_procesados     <- "06_Experimento/datos_procesados"
+ruta_datos_procesados     <- "07_Datos/resultados"
 ruta_resultados           <- "06_Experimento/resultados"
 ruta_salidas_estadisticas <- file.path(ruta_resultados, "salidas_estadisticas")
 
