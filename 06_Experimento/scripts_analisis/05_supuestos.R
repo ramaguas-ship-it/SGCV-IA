989653d7cd87fbbe16aca36f5c514ae2bb2166e6
cdxniel <cbarrionuevof@uteq.edu.ec>
2026-09-10 23:14:51 -0500

    Update data path for processed data directory

--- 06_Experimento/scripts_analisis/05_supuestos.R
+++ 06_Experimento/scripts_analisis/05_supuestos.R
@@ -25,7 +25,7 @@
 #      (ejecutar desde la raiz del repositorio, DESPUES de 04_descriptivos.R)
 # ==============================================================================
 
-ruta_datos_procesados     <- "06_Experimento/datos_procesados"
+ruta_datos_procesados     <- "07_Datos/resultados"
 ruta_resultados           <- "06_Experimento/resultados"
 ruta_salidas_estadisticas <- file.path(ruta_resultados, "salidas_estadisticas")
 
