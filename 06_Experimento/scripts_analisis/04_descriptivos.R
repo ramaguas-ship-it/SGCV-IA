aa122a3986f2e78e57d5a490f6b504981f56a3a7
cdxniel <cbarrionuevof@uteq.edu.ec>
2026-09-10 23:14:27 -0500

    Update 04_descriptivos.R

--- 06_Experimento/scripts_analisis/04_descriptivos.R
+++ 06_Experimento/scripts_analisis/04_descriptivos.R
@@ -17,7 +17,7 @@
 #      (ejecutar desde la raiz del repositorio, DESPUES de 02_limpieza.R)
 # ==============================================================================
 
-ruta_datos_procesados <- "06_Experimento/datos_procesados"
+ruta_datos_procesados <- "07_Datos/resultados"
 ruta_resultados       <- "06_Experimento/resultados"
 ruta_tablas           <- file.path(ruta_resultados, "tablas")
 
