595d9410de4cf936ccf4cc2e4f37b1d39027ad52
cdxniel <cbarrionuevof@uteq.edu.ec>
2026-09-11 14:30:41 -0500

    Update 06_pruebas_hipotesis.R

--- 06_Experimento/scripts_analisis/06_pruebas_hipotesis.R
+++ 06_Experimento/scripts_analisis/06_pruebas_hipotesis.R
@@ -1,3 +1,4 @@
+
 # ==============================================================================
 # 06_Experimento/scripts_analisis/06_pruebas_hipotesis.R
 # ------------------------------------------------------------------------------
@@ -22,7 +23,7 @@
 #      (ejecutar desde la raiz del repositorio, DESPUES de 05_supuestos.R)
 # ==============================================================================
 
-ruta_datos_procesados     <- "07_Datos/resultados"
+ruta_datos_procesados     <- "07_Datos/datos_procesados"
 ruta_resultados           <- "06_Experimento/resultados"
 ruta_salidas_estadisticas <- file.path(ruta_resultados, "salidas_estadisticas")
 
